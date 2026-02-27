854611d9488f3e87e3680ccaac95d0977efddffa
Jad Talbert <jad.talbert@gmail.com>
2026-02-27 14:53:04 -0500

    adding a program for week 6 to my lab

---
+++ week6/loopCounter.cpp
@@ -0,0 +1,21 @@
+/*
+@author: Jad Talbert
+@date: 02/27/2026
+@purpose: this program will count the number of times a loop executes and display the result to the user
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int count = 0; // initialize counter variable
+
+    for (int i = 0; i < 10; i++) { // loop that executes 10 times
+        count++; // increment counter variable
+        cout << "The loop executed " << count << " times." << endl; // display result to user
+    }
+
+    
+    return 0;
+
+}
